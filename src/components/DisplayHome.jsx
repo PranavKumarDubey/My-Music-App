9a9f706e53f89545a44a47192a85804b238feceb
PranavKumarDubey <pranavd709@gmail.com>
2025-08-02 22:39:33 +0530

    testing

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -17,7 +17,7 @@ const DisplayHome = () => {
  </div>
     </div>
       <div className='mb-4'>
-    <h1 className='my-5 font-bold text-2xl'>Today's Top Hits</h1>
+    <h1 className='my-5 font-bold text-2xl'>Today's Top Hits Latest</h1>
     <div className='flex overflow-auto'> 
       {songsData.map((item,index) => (<SongsItems key={index}  name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
  </div>
